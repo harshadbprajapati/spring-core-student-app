d946d8df9a9c5da065dd4fbaab8290fcb536c601
Harshad Prajapati <harshad.b.prajapati@gmail.com>
2024-01-05 10:18:46 +0530

    use of xml configuration

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,18 @@
 package org.example;
 
-import org.example.beans.BTechStudent;
+
 import org.example.beans.Student;
-import org.example.config.AppConfig;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
-        Student student1 = appContext.getBean(Student.class);
+        ApplicationContext appContext = new
+                ClassPathXmlApplicationContext("spring-config.xml");
+        Student student1 = (Student) appContext.getBean("btechStudentBean");
+        Student student2 = (Student) appContext.getBean("btechStudentBean");
         System.out.println(student1.getCurrentStatus());
-//        Student student2 = appContext.getBean(BTechStudent.class);
-//        System.out.println(student2.getCurrentStatus());
-
+        System.out.println(student2.getCurrentStatus());
     }
 }
 
